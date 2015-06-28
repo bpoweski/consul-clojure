a1078da2069ac0fd4a93e643ec037a4d74369857
bpoweski <bpoweski@gmail.com>
2015-06-28 17:43:55 -0500

    move experimental dependencies into dev

--- project.clj
+++ project.clj
@@ -3,11 +3,11 @@
   :url "http://github.com/bpoweski/attache"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[com.stuartsierra/component "0.2.2"]
-                 [camel-snake-kebab "0.3.1" :exclusions [org.clojure/clojure]]
-                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+  :dependencies [[camel-snake-kebab "0.3.1" :exclusions [org.clojure/clojure]]
                  [cheshire "5.5.0"]
-                 [clj-http-lite "0.2.1"]]
+                 [clj-http-lite "0.2.1" :exclusions [org.clojure/clojure]]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.5.1"]
-                                  [org.clojure/tools.trace "0.7.8"]]
+                                  [org.clojure/tools.trace "0.7.8"]
+                                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+                                  [com.stuartsierra/component "0.2.2"]]
                    :jvm-opts ["-XX:MaxPermSize=128M"]}})
