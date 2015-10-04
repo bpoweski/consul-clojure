cbba543a1d86cc01756072575a5540b2f820b441
bpoweski <bpoweski@gmail.com>
2015-10-04 14:54:22 -0500

    update clj-http-lite dependency

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[camel-snake-kebab "0.3.1" :exclusions [org.clojure/clojure com.keminglabs/cljx]]
                  [cheshire "5.5.0"]
-                 [clj-http-lite "0.2.1" :exclusions [org.clojure/clojure]]
+                 [clj-http-lite "0.3.0" :exclusions [org.clojure/clojure]]
                  [com.stuartsierra/component "0.2.2"]]
   :deploy-repositories [["releases" :clojars]]
   :profiles {:dev {:dependencies [[info.sunng/ring-jetty9-adapter "0.8.2" :exclusions [ring/ring-core org.clojure/clojure]]
