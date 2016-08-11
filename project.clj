f0f3825a753213653a5bf2948b782c42d9a26488
bpoweski <bpoweski@gmail.com>
2016-08-11 13:04:17 -0500

    bump clj-http-lite

--- project.clj
+++ project.clj
@@ -5,6 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[camel-snake-kebab "0.3.1" :exclusions [org.clojure/clojure com.keminglabs/cljx]]
                  [cheshire "5.5.0"]
-                 [clj-http-lite "0.2.1" :exclusions [org.clojure/clojure]]]
+                 [clj-http-lite "0.3.0" :exclusions [org.clojure/clojure]]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.7.0"]
                                   [org.clojure/core.async "0.1.346.0-17112a-alpha"]]}})
